6d445483d06dad26fe16d38695f374a9ae4f46fb
Zeya Imran <khanzeya63@gmail.com>
2024-04-18 19:34:53 +0530

    error handling in userController

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,27 +1,23 @@
 import mongoose from "mongoose";
 import { config } from "./config";
 
-
 const connectDb = async () => {
-    try {
-        
-        //after connected
-        mongoose.connection.on("connected", () => {
-            console.log("connected to the database sucessfully");
-        });
+  try {
+    //after connected
+    mongoose.connection.on("connected", () => {
+      console.log("connected to the database sucessfully");
+    });
 
-        // error occured on running server
-        mongoose.connection.on("error", (err)=> {
-            console.log("Error in connecting to database", err);
-        })
+    // error occured on running server
+    mongoose.connection.on("error", (err) => {
+      console.log("Error in connecting to database", err);
+    });
 
-        await mongoose.connect(config.databaseUrl as string);
-
-    } catch (error) {
-        console.error("Failed to connect to database.",error);
-        process.exit(1);
-    }
-}
-
+    await mongoose.connect(config.databaseUrl as string);
+  } catch (error) {
+    console.error("Failed to connect to database.", error);
+    process.exit(1);
+  }
+};
 
 export default connectDb;
--- src/users/userController.ts
+++ src/users/userController.ts
@@ -4,50 +4,47 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
-const createUser = async (req: Request,res: Response ,next:  NextFunction) => {
-    const {name, email, password} = req.body;
+import { User } from "./userTypes";
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
 
+  //validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "all field are required");
+    return next(error);
+  }
 
-    //validation 
-    if(!name || !email || !password){
-        const error = createHttpError(400, "all field are required");
-        return next(error);
+  //database call -checking user already exits or not
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      return next(
+        createHttpError(400, "user already exits, try with another email id")
+      );
     }
+  } catch (error) {
+    return next(createHttpError(500, "Internal server Error"));
+  }
 
-    //database call
-    const user = await userModel.findOne({email});
-    if(user){
-        const error = createHttpError(400,"user already exits, try with another email id");
-        return next(error);
-    }
-    
-
+  //form data process and save into db
+  let newUser: User;
+  try {
     //password hashing
-    const passwordHasing = await bcrypt.hash(password,10);
-
-    //process
-    // try {
-        var newUser = await userModel.create({
-            name,
-            email,
-            password: passwordHasing
-        });
-    // } 
-    // catch (error) {
-    //     createHttpError(400,"user already exits, try with another email id");
-    // }
-
+    const passwordHasing = await bcrypt.hash(password, 10);
+    newUser = await userModel.create({
+      name,
+      email,
+      password: passwordHasing,
+    });
     //Token generation with JWT
-    const accessToken = sign({sub:newUser._id},
-        config.jwtSecret as string, 
-        {
-            expiresIn:'7d',
-            algorithm: "HS256"
-        });
+    const accessToken = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+    res.json({ accessToken: accessToken });
+  } catch (error) {
+    next(createHttpError(500, "Error while creating user"));
+  }
+};
 
-    res.json({"accessToken":accessToken});
-    //response
-
-}
-
-export { createUser}
+export { createUser };
