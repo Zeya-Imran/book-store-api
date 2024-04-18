e2318fbaaea2a878ebf6efcea378f4a03f3fa186
Zeya Imran <khanzeya63@gmail.com>
2024-04-19 03:28:25 +0530

    pdf and coverImage upload code

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,52 @@
+import path from "node:path";
 import { NextFunction, Request, Response } from "express";
-
+import cloudinary from "../config/cloudinary";
+import createHttpError from "http-errors";
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body);
+  console.log("files", req.files);
 
-  res.json({ messag: "something" });
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] }; //declarating of multer types of typescript
+
+  //extracting the file properties from files
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+  try {
+    //uploading coverImage
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: fileName,
+      folder: "book-covers",
+      format: coverImageMimeType,
+    });
+    res.json({ messag: "something" });
+  } catch (error) {
+    return next(createHttpError(500, "Error Occured during the file upload"));
+  }
+
+  try {
+    //extracting properties from pdf
+    const bookFileName = files.file[0].filename;
+    const bookFiledPath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+    const pdfFileMimeType = files.file[0].mimetype.split("/").at(-1);
+
+    //uploading pdf here
+    const bookFileUploadsResult = await cloudinary.uploader.upload(filePath, {
+      resource_type: "raw",
+      filename_override: bookFileName,
+      folder: "book-pdfs",
+      format: pdfFileMimeType,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "error while uploading"));
+  }
 };
 
 export { createBook };
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,11 +3,13 @@ import { config as conf } from "dotenv";
 conf();
 
 const _config = {
-    port: process.env.PORT,
-    databaseUrl: process.env.MONGO_CONNECTION_STRING,
-    env: process.env.NODE_ENV,
-    jwtSecret:process.env.JWT_SECRET
-}
-
+  port: process.env.PORT,
+  databaseUrl: process.env.MONGO_CONNECTION_STRING,
+  env: process.env.NODE_ENV,
+  jwtSecret: process.env.JWT_SECRET,
+  cloudinaryCloudName: process.env.CLOUDINARY_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+};
 
 export const config = Object.freeze(_config);
