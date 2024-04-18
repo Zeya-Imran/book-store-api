1653c05ad56787420f98e447f278154089655555
Zeya Imran <khanzeya63@gmail.com>
2024-04-18 15:52:31 +0530

    usser model setup

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,13 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
-
+import userModel from "./userModel";
 const createUser = async (req: Request,res: Response ,next:  NextFunction) => {
     const {name, email, password} = req.body;
+
+
+    //validation 
     if(!name || !email || !password){
         const error = createHttpError(400, "all field are required");
         return next(error);
     }
 
+    //database call
+    const user = await userModel.findOne({email});
+    if(user){
+        const error = createHttpError(400,"user already exits, try with another email id");
+        next(error);
+    }
+
+
+    //process
+
+    //response
     res.json({message:"from userController"})
 }
 
