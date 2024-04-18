7cf978a75e584426f61a6cf1f90fedcc81d0feab
Zeya Imran <khanzeya63@gmail.com>
2024-04-18 15:30:06 +0530

    body-parser setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 import globalErrorHandler from './middlewares/globalErrorhandler';
 import userRouter from './users/userRouter';
-
+var bodyParser = require('body-parser')
 
 const app = express();
 
 app.get('/',(req,res) => {
     res.json({message: 'welcome to the ebook library'});
 })
+
+
+
+//middlewares (receiving form data and parse)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 
 //routes
--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request,res: Response ,next:  NextFunction) => {
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+        const error = createHttpError(400, "all field are required");
+        return next(error);
+    }
 
     res.json({message:"from userController"})
 }
